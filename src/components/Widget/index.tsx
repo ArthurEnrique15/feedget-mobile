d1242b047f4b2bf304f02f2b21f5fb679fa1443f
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-07 16:53:52 -0300

    feat: add Success component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -9,6 +9,7 @@ import { theme } from '../../theme';
 import { Options } from '../Options';
 import { feedbackTypes } from '../../utils/feedbackTypes';
 import { Form } from '../Form';
+import { Success } from '../Success';
 
 export type FeedbackType = keyof typeof feedbackTypes;
 
@@ -37,9 +38,11 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Form 
+        {/* <Form 
           feedbackType="BUG"
-        />
+        /> */}
+
+        <Success/>
       </BottomSheet>
     </>
   );
